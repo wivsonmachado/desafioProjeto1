3f9f25b2799c50fe18fe1085faf1d9518217ef0c
Wivson Machado <wivsonmachado@gmail.com>
2022-08-04 08:46:20 -0300

    Herança e polimorfismo - parte 1

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,12 +2,8 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-	private String titulo;
-	
-	private String descricao;
-	
 	private LocalDate data;
 	
 	
@@ -16,22 +12,13 @@ public class Mentoria {
 	public Mentoria() {
 	}
 
-	public String getTitulo() {
-		return titulo;
+
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO + 20d;
 	}
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
+	
 	public LocalDate getData() {
 		return data;
 	}
@@ -39,11 +26,15 @@ public class Mentoria {
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
+	
+	
 
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
 	}
+
+
 	
 	
 	
