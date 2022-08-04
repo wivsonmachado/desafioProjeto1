9ece1e5ed82f6adf73a7430a954a6d27bb3580e8
Wivson Machado <wivsonmachado@gmail.com>
2022-08-04 15:58:52 -0300

    Interligando classes de dominio

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -2,7 +2,9 @@ package br.com.dio.desafio;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -21,9 +23,40 @@ public class Main {
 		mentoria1.setData(LocalDate.now());
 		
 		
-		System.out.println(curso1);
-		System.out.println(mentoria1);
-
+//		System.out.println(curso1);
+//		System.out.println(mentoria1);
+		
+		Bootcamp bootcamp = new Bootcamp();
+		
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(mentoria1);
+		
+		Dev devWivson = new Dev();
+		devWivson.setNome("Wivson");
+		devWivson.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos concluídos Wivson" + devWivson.getConteudosInscritos());
+		devWivson.progredir();
+		devWivson.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos inscritos Wivson" + devWivson.getConteudosInscritos());
+		System.out.println("Conteúdos concluídos Wivson" + devWivson.getConteudosConcluidos());
+		System.out.println("XP:" + devWivson.calcularTotalXp());
+		
+		System.out.println("-------------------");
+		
+		Dev devJoao = new Dev();
+		devJoao.setNome("João");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos inscritos João" + devJoao.getConteudosInscritos());
+		devJoao.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos inscritos João" + devJoao.getConteudosInscritos());
+		System.out.println("Conteúdos concluídos João" + devWivson.getConteudosConcluidos());
+		System.out.println("XP:" + devJoao.calcularTotalXp());
+		
+		
 	}
 
 }
